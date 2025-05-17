c71a6b2d379d51b9421cbe6ab31efb181f17694a
Maithili K Hebbar <125458264+maithilikh@users.noreply.github.com>
2025-05-17 15:19:34 +0530

    Update SortColorsDutchFlag.py
    Counting sort

--- Week1/Sorting/SortColorsDutchFlag.py
+++ Week1/Sorting/SortColorsDutchFlag.py
@@ -1,5 +1,18 @@
-def sortColors(self, nums: List[int]) -> None:
-    """
-    Do not return anything, modify nums in-place instead.
-    """
-    nums.sort()
+class Solution:
+    def sortColors(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        ln, d = len(nums), {0:0, 1:0, 2:0}
+        for i in range(ln):
+            d[nums[i]] += 1
+        for i in range(ln):
+            if d[0]:
+                nums[i] = 0
+                d[0] -= 1
+            elif d[1]:
+                nums[i] = 1
+                d[1] -= 1
+            else:
+                nums[i] = 2
+        return
