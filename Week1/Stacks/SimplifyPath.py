a0f97137e98a150d7bd52730a32a3b958bc190e0
Maithili K Hebbar <125458264+maithilikh@users.noreply.github.com>
2025-06-19 20:05:51 +0530

    Updated SimplifyPath.py

--- Week1/Stacks/SimplifyPath.py
+++ Week1/Stacks/SimplifyPath.py
@@ -1,2 +1,29 @@
-def simplifyPath(self, path: str) -> str:
-        
+class Solution:
+    def simplifyPath(self, path: str) -> str:
+        stack = []
+        p, ln = 0, len(path)
+        def handleStack(partStr):
+            if partStr == '.':
+                return                
+            elif partStr == '..':
+                if stack:
+                    stack.pop()
+            else:
+                stack.append(partStr)
+                return
+
+        while p < ln:
+            temp = ""
+            while p < ln and path[p] != '/':
+                temp += path[p]
+                p += 1
+            if temp:
+                handleStack(temp)
+            p += 1
+        if not stack:
+            return '/'
+        else:
+            tbr = ''
+            for i in stack:
+                tbr += ('/'+i)
+            return tbr
