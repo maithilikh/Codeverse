441bfe62c9f861161e7d3f8f4cbf3c1bcc246ca6
Maithili K Hebbar <125458264+maithilikh@users.noreply.github.com>
2025-06-24 19:20:30 +0530

    Update MergeKSortedLists.py

--- Week1/Sorting/MergeKSortedLists.py
+++ Week1/Sorting/MergeKSortedLists.py
@@ -1,2 +1,20 @@
-def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
-        
+class Solution:
+    def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+        if not lists:
+            return None
+        tbr = []
+        for i in lists:
+            if i:
+                p = i
+                while p:
+                    tbr.append(p.val)
+                    p = p.next
+        if not tbr:
+            return None 
+        else:
+            tbr.sort()
+        h = ListNode(tbr[-1], None)
+        for j in range(len(tbr)-2, -1, -1):
+            q = ListNode(tbr[j], h)
+            h = q
+        return h
